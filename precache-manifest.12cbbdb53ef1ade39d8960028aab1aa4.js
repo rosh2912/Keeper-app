78343c5f3350b409bd29eb87139044783f6a42bc
rosh2912 <limbaniroshan603@gmail.com>
2023-06-16 20:01:25 +0530

    Updates

---
+++ precache-manifest.12cbbdb53ef1ade39d8960028aab1aa4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b90b18619503b2ea8a20a0b15fe06e9d",
+    "url": "/keeper-app/index.html"
+  },
+  {
+    "revision": "a4d931c63f47251f68ff",
+    "url": "/keeper-app/static/js/2.260104d4.chunk.js"
+  },
+  {
+    "revision": "8fb19f9a03fb4b4cbaec",
+    "url": "/keeper-app/static/js/main.d9ba788a.chunk.js"
+  },
+  {
+    "revision": "3f5fcd70531cb0f5d766",
+    "url": "/keeper-app/static/js/runtime-main.ad8fc91b.js"
+  }
+]);
